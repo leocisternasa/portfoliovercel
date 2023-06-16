6e297326961cadc4fe674dc8fc68ce7ae2432cc6
Leonardo Cisternas Albornoz <leonardocisternasalbornoz@Leonardos-MacBook-Pro.local>
2023-06-16 17:45:29 +0200

    adding allma dental project

--- pages/cotizador.jsx
+++ pages/cotizador.jsx
@@ -1,46 +1,72 @@
-import Image from 'next/image'
-import React from 'react'
-import cotizadorImg from '../public/assets/projects/cotizadorImg.jpeg'
-import { RiRadioButtonFill } from 'react-icons/ri'
-import Link from 'next/link'
+import Image from "next/image";
+import React from "react";
+import allmaDentalImg from "../public/assets/projects/allmaDental.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
 function cotizador() {
   return (
-    <div className='w-full'>
-      <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'></div>
-        <Image className='absolute z-1' layout='fill' objectFit='cover' src={cotizadorImg} alt='/' />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2>Cotizador</h2>
-          <h3>ReactJs / Vite </h3>
+    <div className="w-full">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/40 z-10"></div>
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={allmaDentalImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2>Dental clinic web site</h2>
+          <h3>ReactJs / NextJs </h3>
         </div>
       </div>
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
-        <div className='col-span-4'>
-          <p>Project</p>
-          <h2>Overview</h2>
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8 mt-8">
+        <div className="col-span-4">
+          <p className="mb-3">Project</p>
+          <h2 className="mb-3">Overview</h2>
           <p>
-          This application is a bank credit quoting system. It delivers the monthly value of the installment that the client must pay based on the term and the amount requested. 
+            This is an ongoing project where I am creating the website of the
+            dental clinic of an orthodontic specialist. The project is deployed
+            in vercel to show the progress to Dr. The technologies used are JS
+            (React), CSS (Tailwind) and HTML.
           </p>
-          <a href='https://github.com/leocisternasa/cotizador' target='_blank' rel="noopener noreferrer">
-          <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+          <a
+            href="https://allma-dental.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Web site</button>
           </a>
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1'>
-              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />React JS</p>
-              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />Vite</p>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" />
+                React JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" />
+                Tailwind Css
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" />
+                HTML
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" />
+                Next JS
+              </p>
             </div>
           </div>
-
         </div>
-        <Link href='/#projets'>
-          <p className='underline cursor-pointer'>Back</p>
+        <Link href="/#projets">
+          <p className="underline cursor-pointer">Back</p>
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default cotizador
+export default cotizador;
