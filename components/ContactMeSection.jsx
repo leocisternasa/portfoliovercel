daaf4412b1a40263ac3c3538a5de252858e8d6e4
leocisternasa <leocisal@gmail.com>
2023-12-29 15:09:03 +0100

    adding information in contactSection

--- components/ContactMeSection.jsx
+++ components/ContactMeSection.jsx
@@ -52,7 +52,7 @@ function ContactMeSection() {
                 <p>Front-End Developer</p>
                 <p className="py-4">
                   I am available for freelance or full-time positions. Contact
-                  me and let`s talk
+                  me and let&apos;s talk
                 </p>
               </div>
 
